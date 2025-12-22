32a00ab8402264186e009690281934c82fb05b12
eshedkal-tech <eshedkal@gmail.com>
2025-12-22 19:45:58 +0200

    refactoring

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+
+from stats import get_num_words
 
 
 main()
