a09ac676f8d968146a57a62b34afab32b00fbf2d
eshedkal-tech <eshedkal@gmail.com>
2025-12-25 18:26:09 +0200

    add molti-book anallsys

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -6,7 +9,7 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -32,5 +35,8 @@ def print_report(book_path, num_words, chars_sorted_list):
 
     print("============= END ===============")
 
+if len(sys.argv) < 2:
+    print ("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 main()
